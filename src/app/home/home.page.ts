98ddf5145f61802af82c2afcbf561cc13d1a4843
t_honma <toru.hom09@gmail.com>
2021-02-01 11:32:25 +0900

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { CompileShallowModuleMetadata } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,84 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+    matches:string[];
+    products:Product[];
+    searchProduct:Product;
+    speech:string;
 
+    constructor(private speechRecongnition: SpeechRecognition) {
+        this.products = [
+            {id:1,name:"アイス",price:200},
+            {id:2,name:"水",price:100},
+            {id:3,name:"おせんべい",price:150}
+        ]
+    }
+
+    startListening(){
+
+        // Check feature available
+        this.speechRecongnition.isRecognitionAvailable()
+        .then((available: boolean) => console.log(available))
+        
+        // Check permission
+        this.speechRecongnition.hasPermission()
+        .then((hasPermission: boolean) => console.log(hasPermission))
+        // Request permissions
+        this.speechRecongnition.requestPermission()
+        .then(
+            () => console.log('Granted'),
+            () => console.log('Denied')
+        )
+
+        let options = {
+            language: 'ja-JP',
+            matches:5,
+            prompt:"",
+            showPopup:true,
+            showPartial:true
+        }
+        this.speechRecongnition.startListening(options).subscribe(matches => {
+            this.matches = matches;
+        });
+
+    }
+
+    stopListening(){
+        //ios
+        this.speechRecongnition.stopListening();
+
+    var cnt=0;
+
+    for (let i = 0; i < this.products.length; i++) {
+        var speech = this.matches[i];
+
+        // console.log(speech);
+        var searchProduct = this.products.filter(products => products.name == speech);
+            if(searchProduct.length){
+                // console.log(searchProduct);
+                this.speech = speech;
+                this.searchProduct = {
+                    id:searchProduct[0].id,
+                    name:searchProduct[0].name,
+                    price:searchProduct[0].price
+                }
+                cnt++;
+            }
+
+    }
+    if(cnt==0){
+
+        this.searchProduct = {
+            id:0,
+            name:'該当なし',
+            price:0
+        }
+    }
+  }
+
+}
+class Product{
+  id:number;
+  name:string;
+  price:number;
 }
